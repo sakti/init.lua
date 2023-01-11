cda5a91acae8c2e0a3c64faffbefec5ba1ba512b
Sakti Dwi Cahyono <sakti.cahyono@stockbit.com>
2023-01-11 14:03:51 +0700

    feat: change keymap for telescope

--- lua/sakti/init.lua
+++ lua/sakti/init.lua
@@ -2,3 +2,5 @@ require("sakti.remap")
 require("sakti.set")
 
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd 'autocmd CmdlineEnter /,? :set hlsearch'
+vim.cmd 'autocmd CmdlineLeave /,? :set nohlsearch'
