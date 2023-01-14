734e54db92fc654c7698b86260b49031ca4aa488
Sakti Dwi Cahyono <me@saktidwicahyono.name>
2023-01-14 13:20:53 +0700

    feat: remap ctrl-o, conflict with zellij
    Signed-off-by: Sakti Dwi Cahyono <me@saktidwicahyono.name>

--- lua/sakti/remap.lua
+++ lua/sakti/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap("t", "<esc><esc>", "<C-\\><C-n>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-o>", { noremap = true })
