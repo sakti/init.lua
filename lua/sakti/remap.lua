db389100a55633da1e645ff98aa8ce5f003936a8
Sakti Dwi Cahyono <sakti.cahyono@stockbit.com>
2023-05-02 11:17:42 +0700

    feat: add keymap for `vim.cmd.NeoTreeShowToggle`
    Signed-off-by: Sakti Dwi Cahyono <sakti.cahyono@stockbit.com>

--- lua/sakti/remap.lua
+++ lua/sakti/remap.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap("t", "<esc><esc>", "<C-\\><C-n>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-o>", { noremap = true })
+vim.keymap.set("n", "<leader>f", vim.cmd.NeoTreeShowToggle)
